18782cc5488b7b63d5f73a14280d51cee94516be
Fred Youhanaie <fyrlang@anydata.co.uk>
2017-12-11 22:40:54 +0000

    extended test program.

--- src/espace_test1.erl
+++ src/espace_test1.erl
@@ -1,9 +1,10 @@
 -module(espace_test1).
 
--export([start/0, test_add2/0]).
+-export([start/0, test_add2/0, test_sums/0]).
 
 start() ->
     espace_cli:eval({espace_test1, test_add2, []}),
+    espace_cli:eval({espace_test1, test_sums, []}),
     espace_cli:out({add, 1, 2}),
     espace_cli:out({add, 2, 3}),
     espace_cli:out({add, 3, 5}).
@@ -13,3 +14,9 @@ test_add2() ->
     {[A, B], _} = espace_cli:in({add, '$1', '$2'}),
     espace_cli:out({sum, A, B, A+B}),
     test_add2().
+
+%% take and print the sum results
+test_sums() ->
+    {[X, Y, Sum], _} = espace_cli:in({sum, '$1', '$2', '$3'}),
+    io:format("~p + ~p = ~p.~n", [X, Y, Sum]),
+    test_sums().

ff4c7410e105c479298a78c0fecee47fbba94d72
Fred Youhanaie <fyrlang@anydata.co.uk>
2018-09-14 15:04:22 +0100

    updated adder1 example to use espace instances.

--- Examples/adder1/adder1.erl
+++ Examples/adder1/adder1.erl
@@ -1,25 +1,40 @@
 -module(adder1).
 
--export([start/0, test_add2/0, test_sums/0]).
+-export([test_add2/1, test_sums/1]).
+-export([start/0, stop/0]).
+-export([start/1, stop/1]).
 
 -spec start() -> 'ok'.
 start() ->
-    espace:eval({adder1, test_add2, []}),
-    espace:eval({adder1, test_sums, []}),
-    espace:out({add, 1, 2}),
-    espace:out({add, 2, 3}),
-    espace:out({add, 3, 5}).
+    start(espace).
+
+-spec start(atom()) -> ok.
+start(Inst_name) ->
+    espace:start(Inst_name),
+    espace:eval(Inst_name, {adder1, test_add2, [Inst_name]}),
+    espace:eval(Inst_name, {adder1, test_sums, [Inst_name]}),
+    espace:out(Inst_name, {add, 1, 2}),
+    espace:out(Inst_name, {add, 2, 3}),
+    espace:out(Inst_name, {add, 3, 5}).
+
+-spec stop() -> 'ok'.
+stop() ->
+    stop(espace).
+
+-spec stop(atom()) -> ok.
+stop(Inst_name) ->
+    espace:stop(Inst_name).
 
 %% add two numbers
--spec test_add2() -> no_return().
-test_add2() ->
-    {[A, B], _} = espace:in({add, '$1', '$2'}),
-    espace:out({sum, A, B, A+B}),
-    test_add2().
+-spec test_add2(atom()) -> no_return().
+test_add2(Inst_name) ->
+    {[A, B], _} = espace:in(Inst_name, {add, '$1', '$2'}),
+    espace:out(Inst_name, {sum, A, B, A+B}),
+    test_add2(Inst_name).
 
 %% take and print the sum results
--spec test_sums() -> no_return().
-test_sums() ->
-    {[X, Y, Sum], _} = espace:in({sum, '$1', '$2', '$3'}),
-    io:format("~n~p + ~p = ~p.~n", [X, Y, Sum]),
-    test_sums().
+-spec test_sums(atom()) -> no_return().
+test_sums(Inst_name) ->
+    {[X, Y, Sum], _} = espace:in(Inst_name, {sum, '$1', '$2', '$3'}),
+    io:format("~n~p: ~p + ~p = ~p.~n", [Inst_name, X, Y, Sum]),
+    test_sums(Inst_name).

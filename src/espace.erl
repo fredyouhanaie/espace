34dedd258e4f0c1f6dc82f38aab9d1e1b10cf21c
Fred Youhanaie <fyrlang@anydata.co.uk>
2019-03-02 23:57:58 +0000

    documentation update.

--- src/espace.erl
+++ src/espace.erl
@@ -3,6 +3,20 @@
 %%% @copyright (C) 2017, Fred Youhanaie
 %%% @doc
 %%% This module provides the client access to the user applications.
+%%%
+%%% In order to run any espace based applications the application
+%%% needs to be started using one of the `start/0,1' functions.
+%%%
+%%% Multiple independent instances of `espace' can be active within
+%%% the same node without interfering with each other. There can be up
+%%% to one unnamed instance, and as many named instance as desired.
+%%% Instance names are short atoms that are appended to the various
+%%% server and table names.
+%%%
+%%% All client functions here have two variants, with and without
+%%% instance name. If no instance name is supplied, or if the instance
+%%% name is `espace', then the unnamed instance is assumed.
+%%%
 %%% @end
 %%% Created : 10 Dec 2017 by Fred Youhanaie <fyrlang@anydata.co.uk>
 %%%-------------------------------------------------------------------
@@ -20,12 +34,7 @@
 
 
 %%--------------------------------------------------------------------
-%% @doc
-%%
-%% Start a new instance of espace.
-%%
-%% start/0 will start the default instance (espace)
-%% start/1 will start a named instance application.
+%% @doc Start a new unnamed instance of espace.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -33,6 +42,11 @@
 start() ->
     start(espace).
 
+%%--------------------------------------------------------------------
+%% @doc Start a new named instance of espace.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec start(atom()) -> ok | {error, term()}.
 start(espace) ->
     application:start(espace);
@@ -52,9 +66,7 @@ start(Inst_name) ->
     application:start(Inst_name).
 
 %%--------------------------------------------------------------------
-%% @doc
-%%
-%% Stop an instance of espace.
+%% @doc Stop the unnamed instance of espace.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -62,14 +74,20 @@ start(Inst_name) ->
 stop() ->
     stop(espace).
 
+%%--------------------------------------------------------------------
+%% @doc Stop a named instance of espace.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec stop(atom()) -> ok | {error, term()}.
 stop(Inst_name) ->
     application:stop(Inst_name).
 
 
 %%--------------------------------------------------------------------
-%% @doc
-%% Perform an "eval" operation via the espace server.
+%% @doc Perform an `eval' operation via the unnamed espace server.
+%%
+%% See `eval/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -77,13 +95,24 @@ stop(Inst_name) ->
 eval(MFA) when is_tuple(MFA) ->
     eval(espace, MFA).
 
+%%--------------------------------------------------------------------
+%% @doc Perform an `eval' operation via a named espace server.
+%%
+%% Currently `eval' behaves similar to `worker', however this will
+%% change in the very near future. Please use the `worker/1,2'
+%% functions instead of `eval/1,2'.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec eval(atom(), tuple()) -> ok.
 eval(Inst_name, MFA) when is_tuple(MFA) ->
     tspool_srv:espace_eval(Inst_name, MFA).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% start a new worker process.
+%% start a new worker process via the unnamed espace server.
+%%
+%% See `worker/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -91,13 +120,35 @@ eval(Inst_name, MFA) when is_tuple(MFA) ->
 worker(MFA) when is_tuple(MFA) ->
     worker(espace, MFA).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% start a new worker process via a named espace server.
+%%
+%% The function expects a single tuple as argument, which can have one
+%% of three forms:
+%%
+%% <ul>
+%%
+%% <li>A `{Mod, Fun, Args}' triple, e.g. `{adder1, test_add2, []}'.</li>
+%%
+%% <li>A single fun, e.g. `{fun () -> adder1:test_add2() end}'.</li>
+%%
+%% <li>A string containing a fun, e.g. `"fun () -> adder1:test_add2()
+%% end."'. This is mainly for use in espace input files.</li>
+%%
+%% </ul>
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec worker(atom(), tuple()) -> ok.
 worker(Inst_name, MFA) when is_tuple(MFA) ->
     tspool_srv:espace_worker(Inst_name, MFA).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Perform an "in" operation via the espace server.
+%% Perform an `in' operation via the unnamed espace server.
+%%
+%% See `in/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -105,13 +156,41 @@ worker(Inst_name, MFA) when is_tuple(MFA) ->
 in(Pattern) ->
     in(espace, Pattern).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Perform an `in' operation via a named espace server.
+%%
+%% For details of possible patterns see the match_spec in
+%% `ets:match/2,3'. `Pattern' must be a tuple. It may consist of
+%% expressions, or it may contain `$N' pattern variables, where N>0.
+%%
+%% If the pattern does not match a tuple in the tuple space, then the
+%% call will block until such a pattern is added to the tuple
+%% space.
+%%
+%% If a match is found, the tuple is returned as a pair `{List,
+%% Tuple}', where `Tuple' is the whole tuple, and `List' is a,
+%% possibly empty, list containing the matched `$N' pattern
+%% variables. The items in the list will be ordered by the `$N'
+%% variable numbers.
+%%
+%% For example the tuple `{add, 34, 88}' will match the pattern {add,
+%% '$2', '$1'}, and the returned result will be '{[88, 34], {add, 34,
+%% 88}}'. Note the order of the numbers in the list.
+%%
+%% The matched tuple will be removed from the tuple space.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec in(atom(), tuple()) -> {list(), tuple()}.
 in(Inst_name, Pattern) ->
     tspool_srv:espace_in(Inst_name, Pattern).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Perform an "inp" operation via the espace server.
+%% Perform an `inp' operation via the unnamed espace server.
+%%
+%% See `inp/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -119,13 +198,29 @@ in(Inst_name, Pattern) ->
 inp(Pattern) when is_tuple(Pattern) ->
     inp(espace, Pattern).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Perform an `inp' operation via a named espace server.
+%%
+%% The `inp/1,2' functions are the non-blocking versions of
+%% `in/1,2`. If a match is not found in the first instance, then the
+%% atom `nomatch' is returned. If a match is found, then it will be
+%% removed from the tuple space and returned as a `{List, Tuple}'
+%% pair.
+%%
+%% See `in/1,2' for details and examples of the match patterns.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec inp(atom(), tuple()) -> nomatch | {list(), tuple()}.
 inp(Inst_name, Pattern) when is_tuple(Pattern) ->
     tspool_srv:espace_inp(Inst_name, Pattern).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Perform an "out" operation via the espace server.
+%% Perform an `out' operation via the unnamed espace server.
+%%
+%% See `out/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -133,13 +228,24 @@ inp(Inst_name, Pattern) when is_tuple(Pattern) ->
 out(Tuple) when is_tuple(Tuple) ->
     out(espace, Tuple).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Perform an `out' operation via a named espace server.
+%%
+%% The `Tuple' supplied as argument will be added to the tuple
+%% space. Duplicate tuples are allowed in the tuple space.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec out(atom(), tuple()) -> ok.
 out(Inst_name, Tuple) when is_tuple(Tuple) ->
     tspool_srv:espace_out(Inst_name, Tuple).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Perform a "rd" operation via the espace server.
+%% Perform a `rd' operation via the unnamed espace server.
+%%
+%% See `rd/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -147,13 +253,25 @@ out(Inst_name, Tuple) when is_tuple(Tuple) ->
 rd(Pattern) when is_tuple(Pattern) ->
     rd(espace, Pattern).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Perform a `rd' operation via a named espace server.
+%%
+%% The `rd/1,2' functions behave in the same manner as `in/1,2' except
+%% that when a matching tuple is found, it will not be removed from
+%% the tuple space.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec rd(atom(), tuple()) -> {list(), tuple()}.
 rd(Inst_name, Pattern) when is_tuple(Pattern) ->
     tspool_srv:espace_rd(Inst_name, Pattern).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Perform a "rdp" operation via the espace server.
+%% Perform a `rdp' operation via the unnamed espace server.
+%%
+%% See `rdp/2' for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -161,14 +279,26 @@ rd(Inst_name, Pattern) when is_tuple(Pattern) ->
 rdp(Pattern) when is_tuple(Pattern) ->
     rdp(espace, Pattern).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Perform a `rdp' operation via a named espace server.
+%%
+%% The functions `rdp/1,2' are the non-blocking versions of
+%% `rd/1,2'. That is, if a match is not found, then `nomatch' is
+%% returned, and if a match is found the matching tuple will be
+%% return, but it will not be removed from the tuple space.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec rdp(atom(), tuple()) -> nomatch | {list(), tuple()}.
 rdp(Inst_name, Pattern) when is_tuple(Pattern) ->
     tspool_srv:espace_rdp(Inst_name, Pattern).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Read and process an espace input file.
-%% The file should be a valid Erlang terms file.
+%% Read and process an espace input file via the unnamed server.
+%%
+%% See infile/2 for details.
 %%
 %% @end
 %%--------------------------------------------------------------------
@@ -176,20 +306,30 @@ rdp(Inst_name, Pattern) when is_tuple(Pattern) ->
 infile(File) ->
     infile(espace, File).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Read and process an espace input file via a named server.
+%%
+%% The file should be a valid Erlang terms file.
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec infile(atom(), file:name_all()) -> ok.
 infile(Inst_name, File) ->
     {ok, Terms} = file:consult(File),
     do_esp(Inst_name, Terms).
 
-
 %%--------------------------------------------------------------------
 %% @doc
+%% Convert an instance name to longer prefixed name.
 %%
-%% Convert an instance name to longer prefixed name. This is used for
-%% obtaining the instance specific server/table names.
+%% This is used for obtaining the instance specific server/table
+%% names. For example `inst_to_name(espace_sup, aaa)' will return
+%% `espace_sup_aaa'.
 %%
 %% If the instance name is `espace', then the prefix is returned
-%% without an instance name suffix.
+%% without an instance name suffix. For example
+%% `inst_to_name(espace_sup, espace)' will return `espace_sup'.
 %%
 %% @end
 %%--------------------------------------------------------------------

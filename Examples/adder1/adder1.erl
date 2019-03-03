3c49afbccc3e3caa4b6529fd3b9727f1ccf325f1
Fred Youhanaie <fyrlang@anydata.co.uk>
2019-03-03 00:20:14 +0000

    adder1 example updated.
    - converted eval/1,2 to worker/1,2.
    - local workers can use unnamed and named servers.

--- Examples/adder1/adder1.erl
+++ Examples/adder1/adder1.erl
@@ -1,6 +1,7 @@
 -module(adder1).
 
 -export([test_add2/1, test_sums/1]).
+-export([test_add2/0, test_sums/0]).
 -export([start/0, stop/0]).
 -export([start/1, stop/1]).
 
@@ -11,8 +12,8 @@ start() ->
 -spec start(atom()) -> ok.
 start(Inst_name) ->
     espace:start(Inst_name),
-    espace:eval(Inst_name, {adder1, test_add2, [Inst_name]}),
-    espace:eval(Inst_name, {adder1, test_sums, [Inst_name]}),
+    espace:worker(Inst_name, {adder1, test_add2, [Inst_name]}),
+    espace:worker(Inst_name, {adder1, test_sums, [Inst_name]}),
     espace:out(Inst_name, {add, 1, 2}),
     espace:out(Inst_name, {add, 2, 3}),
     espace:out(Inst_name, {add, 3, 5}).
@@ -25,14 +26,24 @@ stop() ->
 stop(Inst_name) ->
     espace:stop(Inst_name).
 
-%% add two numbers
+%% add two numbers (unnamed server)
+-spec test_add2() -> no_return().
+test_add2() ->
+    test_add2(espace).
+
+%% add two numbers (named server)
 -spec test_add2(atom()) -> no_return().
 test_add2(Inst_name) ->
     {[A, B], _} = espace:in(Inst_name, {add, '$1', '$2'}),
     espace:out(Inst_name, {sum, A, B, A+B}),
     test_add2(Inst_name).
 
-%% take and print the sum results
+%% take and print the sum results (unnamed server)
+-spec test_sums() -> no_return().
+test_sums() ->
+    test_sums(espace).
+
+%% take and print the sum results (named server)
 -spec test_sums(atom()) -> no_return().
 test_sums(Inst_name) ->
     {[X, Y, Sum], _} = espace:in(Inst_name, {sum, '$1', '$2', '$3'}),
